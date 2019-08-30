691346f09f9739d7fdb4e24fb6ad60d5c5be931e
Prescott Sun <sunprescott@gmail.com>
2019-08-30 15:45:31 -0400

    Python exercises from Day 2

---
+++ madlib.py
@@ -0,0 +1,4 @@
+name = input('What is name? ')
+subject = input('What is subject? ')
+
+print(name + "\'s favorite subject in school is " + subject)
---
+++ prompt_for_number.py
@@ -0,0 +1,4 @@
+user_input = int(input("Enter a number: "))
+# print(type(user_input))
+result = user_input * user_input
+print(result)
